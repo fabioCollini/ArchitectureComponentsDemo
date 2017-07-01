d8fd8eacfa609fabacf96e14dc33a0f037a31ca0
fabioCollini <fabio.collini@gmail.com>
2017-07-01 16:20:55 +0200

    New typealias UiAction to simplify methods definitions

--- app/src/main/java/it/codingjam/github/util/UiActionsLiveData.kt
+++ app/src/main/java/it/codingjam/github/util/UiActionsLiveData.kt
@@ -7,27 +7,29 @@ import android.support.annotation.MainThread
 import android.support.v4.app.FragmentActivity
 import java.util.*
 
+typealias UiAction = (FragmentActivity) -> Unit
+
 class UiActionsLiveData {
-    private val delegate = MutableLiveData<List<(FragmentActivity) -> Unit>>()
+    private val delegate = MutableLiveData<List<UiAction>>()
 
-    private var list: MutableList<(FragmentActivity) -> Unit> = ArrayList()
+    private var list: MutableList<UiAction> = ArrayList()
 
-    fun execute(action: (FragmentActivity) -> Unit) {
+    fun execute(action: UiAction) {
         list.add(action)
         delegate.value = list
     }
 
-    operator fun invoke(action: (FragmentActivity) -> Unit) {
+    operator fun invoke(action: UiAction) {
         execute(action)
     }
 
-    fun observe(owner: LifecycleOwner, executor: ((FragmentActivity) -> Unit) -> Unit) =
+    fun observe(owner: LifecycleOwner, executor: (UiAction) -> Unit) =
             delegate.observe(owner, Observer {
                 list.forEach { executor(it) }
                 list = ArrayList()
             })
 
-    @MainThread fun observeForever(executor: ((FragmentActivity) -> Unit) -> Unit) =
+    @MainThread fun observeForever(executor: (UiAction) -> Unit) =
             delegate.observeForever {
                 list.forEach { executor(it) }
                 list = ArrayList()
