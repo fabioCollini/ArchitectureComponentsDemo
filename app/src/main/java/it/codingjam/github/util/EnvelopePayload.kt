5d8d7bb58e8977e9292bdf398a2477e7951d6c76
fabioCollini <fabio.collini@gmail.com>
2017-08-13 11:47:46 +0200

    Fixed warning

--- app/src/main/java/it/codingjam/github/util/EnvelopePayload.kt
+++ app/src/main/java/it/codingjam/github/util/EnvelopePayload.kt
@@ -1,12 +1,10 @@
 package it.codingjam.github.util
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy.RUNTIME
 
 /**
  * An annotation for identifying the payload that we want to extract from an API response wrapped in
  * an envelope object.
  */
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-@Retention(RUNTIME)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class EnvelopePayload(val value: String = "")
