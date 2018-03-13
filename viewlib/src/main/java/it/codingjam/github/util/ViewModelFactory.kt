34bd2c0feb066d3c98cda8994ab13ba884524bb8
Fabio Collini <fabio.collini@gmail.com>
2018-03-13 22:06:57 +0100

    ViewModelFactory with Activity parameter

--- viewlib/src/main/java/it/codingjam/github/util/ViewModelFactory.kt
+++ viewlib/src/main/java/it/codingjam/github/util/ViewModelFactory.kt
@@ -4,15 +4,31 @@ import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import javax.inject.Provider
 
 class ViewModelFactory(val testProvider: (() -> ViewModel)? = null) {
-    inline operator fun <VM : ViewModel> invoke(fragment: Fragment, provider: Provider<VM>, crossinline init: (VM) -> Unit = {}): VM {
-        return ViewModelProviders.of(fragment, object : ViewModelProvider.Factory {
+
+    inline fun <reified VM : ViewModel> factory(
+            provider: Provider<VM>, crossinline init: (VM) -> Unit
+    ): ViewModelProvider.Factory {
+        return object : ViewModelProvider.Factory {
             override fun <T1 : ViewModel> create(aClass: Class<T1>): T1 {
                 val viewModel = testProvider?.invoke() as VM? ?: provider.get()
                 return viewModel.also(init) as T1
             }
-        }).get(ViewModel::class.java) as VM
+        }
+    }
+
+    inline operator fun <reified VM : ViewModel> invoke(
+            fragment: Fragment, provider: Provider<VM>, crossinline init: (VM) -> Unit = {}
+    ): VM {
+        return ViewModelProviders.of(fragment, factory(provider, init)).get(VM::class.java)
+    }
+
+    inline operator fun <reified VM : ViewModel> invoke(
+            activity: FragmentActivity, provider: Provider<VM>, crossinline init: (VM) -> Unit = {}
+    ): VM {
+        return ViewModelProviders.of(activity, factory(provider, init)).get(VM::class.java)
     }
 }
