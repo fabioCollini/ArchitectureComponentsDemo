6d257e89e02217ecfb680267b53507531890eadb
fabioCollini <fabio.collini@gmail.com>
2018-09-28 08:37:02 +0200

    Fix coroutines usage

--- core/src/main/java/it/codingjam/github/core/GithubInteractor.kt
+++ core/src/main/java/it/codingjam/github/core/GithubInteractor.kt
@@ -1,7 +1,7 @@
 package it.codingjam.github.core
 
-import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.coroutineScope
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,16 +13,16 @@ class GithubInteractor @Inject constructor(private val githubRepository: GithubR
 
     suspend fun searchNextPage(query: String, nextPage: Int) = githubRepository.searchNextPage(query, nextPage)
 
-    suspend fun loadRepo(owner: String, name: String): RepoDetail {
-        val repo = async(CommonPool) { githubRepository.getRepo(owner, name) }
-        val contributors = async(CommonPool) { githubRepository.getContributors(owner, name) }
+    suspend fun loadRepo(owner: String, name: String): RepoDetail = coroutineScope {
+        val repo = async { githubRepository.getRepo(owner, name) }
+        val contributors = async { githubRepository.getContributors(owner, name) }
 
-        return RepoDetail(repo.await(), contributors.await())
+        RepoDetail(repo.await(), contributors.await())
     }
 
-    suspend fun loadUserDetail(login: String): UserDetail {
-        val userDeferred = async(CommonPool) { githubRepository.loadUser(login) }
-        val reposDeferred = async(CommonPool) { githubRepository.loadRepos(login) }
-        return UserDetail(userDeferred.await(), reposDeferred.await())
+    suspend fun loadUserDetail(login: String): UserDetail = coroutineScope {
+        val userDeferred = async { githubRepository.loadUser(login) }
+        val reposDeferred = async { githubRepository.loadRepos(login) }
+        UserDetail(userDeferred.await(), reposDeferred.await())
     }
 }
